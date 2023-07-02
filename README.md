eb3e6529c4c29ded9b1e4339a3e76a1f95b1c6da
DevJuan15 <Juangonmor15@gmail.com>
2023-07-02 13:24:31 -0500

    Agregadas varias funciones a la aplicacion

--- README.md
+++ README.md
@@ -19,4 +19,20 @@ Changes to be committed:
   (use "git restore --staged <file>..." to unstage)
         new file:   app.py
 ``` 
-3. 
+3. Para agregar el archivo app.py a los cambios de github se debe ejecutar el siguiente comando
+```console
+git add .
+git status
+```
+el resultado de correr este codigo es:
+```console
+ubuntu@DESKTOP-RD2D6DN:~/proyectos/jpgym$ git add .
+ubuntu@DESKTOP-RD2D6DN:~/proyectos/jpgym$ git status
+On branch main
+Your branch is up to date with 'origin/main'.
+
+Changes to be committed:
+  (use "git restore --staged <file>..." to unstage)
+        modified:   README.md
+        new file:   app.py
+```
--- app.py
+++ app.py
@@ -13,14 +13,15 @@ membresias = {
 costo_premium = 100
 costo_basica = 50
 
-print("¡Bienvenido al gimnasio bro!")
+# print("¡Bienvenido al gimnasio bro!")
 
 # LISTAS
 clientes_premium = []
 clientes_basica = []
 
-# TUPLAS
+#TUPLAS
 horarios = ("Mañana", "Tarde", "Noche")
+
 
 # SETS
 actividades = {"Yoga", "Spinning", "Zumba", "CrossFit"}
@@ -33,42 +34,74 @@ precios_actividades = {
     "CrossFit": 25
 }
 
-# USO DE CONDICIONALES (IF, ELIF)
+#USO DE CONDICIONALES (IF, ELIF)
 def calcular_costo_membresia(cliente):
     if membresias[cliente] == "Premium":
         return costo_premium
     elif membresias[cliente] == "Básica":
         return costo_basica
 
-cliente_elegido = input("Ingrese el nombre del cliente: ")
+# cliente_elegido = input("Ingrese el nombre del cliente: ")
 
-if cliente_elegido in membresias:
-    costo_membresia = calcular_costo_membresia(cliente_elegido)
-    print(f"El costo de la membresía de {cliente_elegido} es: ${costo_membresia}")
-else:
-    print("El cliente ingresado no existe en el sistema.")
+# if cliente_elegido in membresias:
+#     costo_membresia = calcular_costo_membresia(cliente_elegido)
+#     print(f"El costo de la membresía de {cliente_elegido} es: ${costo_membresia}")
+# else:
+#     print("El cliente ingresado no existe en el sistema.")
 
-# USO DE BUCLES, AGREGAMOS LOS CLIENTES A LAS LISTAS SEGUN SU MEMBRESIA
-for cliente, tipo_membresia in membresias.items():
-    if tipo_membresia == "Premium":
-        clientes_premium.append(cliente)
-    elif tipo_membresia == "Básica":
-        clientes_basica.append(cliente)
+# # USO DE BUCLES, AGREGAMOS LOS CLIENTES A LAS LISTAS SEGUN SU MEMBRESIA
+# for cliente, tipo_membresia in membresias.items():
+#     if tipo_membresia == "Premium":
+#         clientes_premium.append(cliente)
+#     elif tipo_membresia == "Básica":
+#         clientes_basica.append(cliente)
 
-# Funciones, SE CALCULA EL COSTO TOTAL
+# # Funciones, SE CALCULA EL COSTO TOTAL
 def calcular_costo_actividades(actividades_elegidas):
-    costo_total = 0
-    for actividad in actividades_elegidas:
-        costo_total += precios_actividades[actividad]
+    costo_total = precios_actividades[actividades_elegidas]
+   
+    # costo_total = 0
+    # for actividad in actividades_elegidas:
+    #     costo_total += precios_actividades[actividad]
     return costo_total
 
-#ESTE ES EL USO DE LAS FUNCIONES Y CONDICIONALES, NOS PEDIRA EL NOMBRE Y DARA LOS DATOS RECOLECTADOS
-actividades_elegidas = ["Yoga", "Spinning", "CrossFit"]
-costo_actividades = calcular_costo_actividades(actividades_elegidas)
+# #ESTE ES EL USO DE LAS FUNCIONES Y CONDICIONALES, NOS PEDIRA EL NOMBRE Y DARA LOS DATOS RECOLECTADOS
+# actividades_elegidas = ["Yoga", "Spinning", "CrossFit"]
+# costo_actividades = calcular_costo_actividades(actividades_elegidas)
 
-if cliente_elegido in membresias:
-    actividades_elegidas = ["Yoga", "Spinning", "CrossFit"]
-    costo_actividades = calcular_costo_actividades(actividades_elegidas)
-    print(f"El costo total de las actividades elegidas es: ${costo_actividades}")
-else:
-    print("No existe ningún valor en actividades elegidas")
+# if cliente_elegido in membresias:
+#     actividades_elegidas = ["Yoga", "Spinning", "CrossFit"]
+#     costo_actividades = calcular_costo_actividades(actividades_elegidas)
+#     print(f"El costo total de las actividades elegidas es: ${costo_actividades}")
+# else:
+#     print("No existe ningún valor en actividades elegidas")
+    
+    
+    
+    
+import streamlit as st
+
+def main():
+    st.title("GIMNASIO JP SCHWARZENEGGER")
+    
+    
+    # Seccio ndel usuario y el costo de la membresia
+    nombre_usuario = st.selectbox('Selecciona tu nombre:', membresias)
+    
+    costo_calculado = calcular_costo_membresia (nombre_usuario)
+    
+    st.info(f"costo calculado para el usuario es de: {costo_calculado}")
+
+
+    #Seccion de las actividades
+    nombre_actividad = st.selectbox("seleccione el nombre de la actividad",precios_actividades)
+    costo_actividad_calculado = calcular_costo_actividades (nombre_actividad)
+    st.info (f"el costo calculado de la actividad es de {costo_actividad_calculado}")   
+    
+    mensaje = f'Hola, {nombre_usuario}!'
+    
+   
+    st.write(mensaje)
+
+if __name__ == '__main__':
+    main()
